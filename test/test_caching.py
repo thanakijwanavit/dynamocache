3b88b543a16d8afa424aa655265c1a351d3d7a23
Nic Wanavit <nwanavit@gmail.com>
2023-03-11 13:33:55 +0800

    add test timer

--- test/test_caching.py
+++ test/test_caching.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
-from src.dynamocache import Dynamocache
+from src.dynamocache.dynamocache import Dynamocache
+import time, pytest
 
 
 class TestDynamocache(unittest.TestCase):
@@ -19,6 +20,19 @@ class TestDynamocache(unittest.TestCase):
     def test_create_table(self):
         dynamocache = Dynamocache('test-cache')
         dynamocache.create_table()
+    
+    @pytest.mark.timeout(3)
+    def test_timing(self):
+        dynamocache = Dynamocache('test-cache')
+        memoize_decorator = dynamocache.memoize(ttl_seconds=5)
+        @memoize_decorator
+        def slow_function():
+            time.sleep(1)
+            return 42
+
+        for _ in range(10):
+            slow_function()
+        
 
 
 if __name__=='__main__':
